f1d052fbcdee907da4f6d30dfa653f34addc85c9
Juan Ramos <jramos32@ucol.mx>
2020-03-14 13:45:09 -0600

    encapsulado

--- fecha.js
+++ fecha.js
@@ -1,36 +1,38 @@
+const mesAño =["Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sept","Oct","Nov","Dic"]
+const diaSemana=["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"]
 export default class Fecha{
     constructor(dia,mes,año){
   
-        this.fecha=new Date(año,mes -1, dia);
-        this.mesAño=["Ene","Feb","Mar","Abr","May","Jun","Jul","Ago","Sept","Oct","Nov","Dic"]
-        this.diaSemana=["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"]
-        this.difFecha= new Date (Date.now() - this.fecha)
+        this._fecha=new Date(año,mes -1, dia);
+        this._difFecha= new Date (Date.now() - this.fecha)
     }
     getAños(){
         let canAños = 1000 * 60 * 60 * 24 * 365;
-        let totalAños = Math.trunc(this.difFecha/canAños);
+        let totalAños = Math.trunc(this._difFecha/canAños);
         return (`${totalAños}`);
     }
     getMeses(){
         let canMeses = (1000 * 60 * 60 * 24 * 365)/12;
-        let totalMeses = Math.trunc(this.difFecha/canMeses);
+        let totalMeses = Math.trunc(this._difFecha/canMeses);
         return (`${totalMeses}`);
     }
     getSemanas(){
         let canSemanas = (1000 * 60 * 60 * 24 * 365)/52;
-        let totalSemanas = Math.trunc(this.difFecha/canSemanas);
+        let totalSemanas = Math.trunc(this._difFecha/canSemanas);
         return (`${totalSemanas}`);
     }
     getDias(){
         let canDias = 1000 * 60 * 60 * 24;
-        let totalDias = Math.trunc(this.difFecha/canDias);
+        let totalDias = Math.trunc(this._difFecha/canDias);
         return (`${totalDias}`);
     }
     getFecha(){
-        return(`${this.fecha.getDate()}/${this.mesAño[this.fecha.getMonth()]}/${this.fecha.getFullYear()}`);
+        let mes = mesAño[this._fecha.getMonth()]
+        return(`${this._fecha.getDate()}/${mes}/${this._fecha.getFullYear()}`);
     }
     getDiaFecha(){
-        return(`${this.diaSemana[this.fecha.getDay()]}`);
+        let dia = diaSemana[this._fecha.getDay()]
+        return(`${dia}`);
 
     }
     
