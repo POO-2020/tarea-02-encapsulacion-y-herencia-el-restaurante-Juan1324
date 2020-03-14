a9ce4402b00adcccea68dcb6f4992b3f2e57f9b6
Juan Ramos <jramos32@ucol.mx>
2020-03-14 15:03:59 -0600

    encapsulado

--- pedido.js
+++ pedido.js
@@ -1,43 +1,41 @@
 import Precio from "./precio.js";
-import Cliente from "./cliente.js";
-import Producto from "./producto.js";
-import ElementoPedido from "./elementoPedido.js";
 
 export default class Pedido{
-    constructor(fecha,hora,cliente){
-        this.fecha = fecha;
-        this.hora = hora;
-        this.cliente = cliente;
-        this.NuelementosPedidos = []
+    constructor({fecha,hora,cliente,numeroPedido}){
+        this._fecha = fecha;
+        this._hora = hora;
+        this._cliente = cliente;
+        this._numeroPedido= numeroPedido;
+        this._NuelementosPedidos = []
 
     }
     getNumeroElementos(){
-        return (`${this.NuelementosPedidos.length}`);
+        return (`${this._NuelementosPedidos.length}`);
     }
     getNumeroProductos(){
         var productoNum = 0;
-        this.NuelementosPedidos.forEach(elemento =>{
+        this._NuelementosPedidos.forEach(elemento =>{
             productoNum += elemento.cantidad 
         })
         return (productoNum);
     }
     getCostoTotal(){
         var costo = 0;
-        this.NuelementosPedidos.forEach(elemento1 => {
+        this._NuelementosPedidos.forEach(elemento1 => {
             costo += elemento1.cantidad * elemento1.producto.precio.valor
         });
         return (`${new Precio(costo).getPrecio()}`);
     }
     agregarElemento(elemento1){
-        return (`${this.NuelementosPedidos.push(elemento1)}`);
+        return (`${this._NuelementosPedidos.push(elemento1)}`);
     }      
     listarElementos(){
         console.log("Listado de los Platillos")
-        this.NuelementosPedidos.forEach(elemento =>{
+        this._NuelementosPedidos.forEach(elemento =>{
             console.log(elemento.getDescripcion());       
         })}
         getResumen(){
-            return (`${this.fecha.getFecha()} ${this.hora.getFormato12()} \n- ${this.getNumeroElementos()} Elementos con ${this.getNumeroProductos()} Productos.`);
+            return (`${this._fecha.getFecha()} ${this._hora.getFormato12()} \n- ${this.getNumeroElementos()} Elementos con ${this.getNumeroProductos()} Productos.`);
         }
     
     }
